062b0e35d66f347c926f7fa398c3ef746a254899
Tahmina Rahman <tahminarahmansurovi@gmail.com>
2022-09-06 23:14:19 +0600

    pattern problem

---
+++ DataStructure&Algo_practice/src/Patterns/SolidRectangle.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.*;
+public class SolidRectangle {
+	
+	public static void main(String args[]) {
+		
+		int m = 4, n = 5;
+		
+		
+		for(int i=1;i<=m;i++) {
+			
+			for(int j = 1;j<=n;j++) {
+				
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
